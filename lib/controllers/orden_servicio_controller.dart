e246f90d12c4410e3c66b827017b65ebb220cc4d
angelSantori <antonyo.da.mm@gmail.com>
2025-08-18 21:12:58 -0700

    Orden Servicio Controller - Quitar calle y colonia - Agregar comentario

--- lib/controllers/orden_servicio_controller.dart
+++ lib/controllers/orden_servicio_controller.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 // Librerías
 import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+
 import 'package:jmas_gestion/service/auth_service.dart';
 
 class OrdenServicioController {
@@ -152,65 +155,61 @@ class OrdenServicioController {
 class OrdenServicio {
   int? idOrdenServicio;
   String? folioOS;
-  String? contactoOS;
   String? fechaOS;
   bool? materialOS;
   String? estadoOS;
   String? prioridadOS;
+  String? contactoOS;
+  String? comentarioOS;
   int? idUser;
   int? idPadron;
   int? idTipoProblema;
   int? idMedio;
-  int? idCalle;
-  int? idColonia;
   int? idUserAsignado;
   OrdenServicio({
     this.idOrdenServicio,
     this.folioOS,
-    this.contactoOS,
     this.fechaOS,
     this.materialOS,
     this.estadoOS,
     this.prioridadOS,
+    this.contactoOS,
+    this.comentarioOS,
     this.idUser,
     this.idPadron,
     this.idTipoProblema,
     this.idMedio,
-    this.idCalle,
-    this.idColonia,
     this.idUserAsignado,
   });
 
   OrdenServicio copyWith({
     int? idOrdenServicio,
     String? folioOS,
-    String? contactoOS,
     String? fechaOS,
     bool? materialOS,
     String? estadoOS,
     String? prioridadOS,
+    String? contactoOS,
+    String? comentarioOS,
     int? idUser,
     int? idPadron,
     int? idTipoProblema,
     int? idMedio,
-    int? idCalle,
-    int? idColonia,
     int? idUserAsignado,
   }) {
     return OrdenServicio(
       idOrdenServicio: idOrdenServicio ?? this.idOrdenServicio,
       folioOS: folioOS ?? this.folioOS,
-      contactoOS: contactoOS ?? this.contactoOS,
       fechaOS: fechaOS ?? this.fechaOS,
       materialOS: materialOS ?? this.materialOS,
       estadoOS: estadoOS ?? this.estadoOS,
       prioridadOS: prioridadOS ?? this.prioridadOS,
+      contactoOS: contactoOS ?? this.contactoOS,
+      comentarioOS: comentarioOS ?? this.comentarioOS,
       idUser: idUser ?? this.idUser,
       idPadron: idPadron ?? this.idPadron,
       idTipoProblema: idTipoProblema ?? this.idTipoProblema,
       idMedio: idMedio ?? this.idMedio,
-      idCalle: idCalle ?? this.idCalle,
-      idColonia: idColonia ?? this.idColonia,
       idUserAsignado: idUserAsignado ?? this.idUserAsignado,
     );
   }
@@ -219,90 +218,81 @@ class OrdenServicio {
     return <String, dynamic>{
       'idOrdenServicio': idOrdenServicio,
       'folioOS': folioOS,
-      'contactoOS': contactoOS,
       'fechaOS': fechaOS,
       'materialOS': materialOS,
       'estadoOS': estadoOS,
       'prioridadOS': prioridadOS,
+      'contactoOS': contactoOS,
+      'comentarioOS': comentarioOS,
       'idUser': idUser,
       'idPadron': idPadron,
       'idTipoProblema': idTipoProblema,
       'idMedio': idMedio,
-      'idCalle': idCalle,
-      'idColonia': idColonia,
       'idUserAsignado': idUserAsignado,
     };
   }
 
   factory OrdenServicio.fromMap(Map<String, dynamic> map) {
     return OrdenServicio(
-      idOrdenServicio:
-          map['idOrdenServicio'] != null ? map['idOrdenServicio'] as int : null,
+      idOrdenServicio: map['idOrdenServicio'] != null ? map['idOrdenServicio'] as int : null,
       folioOS: map['folioOS'] != null ? map['folioOS'] as String : null,
-      contactoOS:
-          map['contactoOS'] != null ? map['contactoOS'] as String : null,
       fechaOS: map['fechaOS'] != null ? map['fechaOS'] as String : null,
       materialOS: map['materialOS'] != null ? map['materialOS'] as bool : null,
       estadoOS: map['estadoOS'] != null ? map['estadoOS'] as String : null,
-      prioridadOS:
-          map['prioridadOS'] != null ? map['prioridadOS'] as String : null,
+      prioridadOS: map['prioridadOS'] != null ? map['prioridadOS'] as String : null,
+      contactoOS: map['contactoOS'] != null ? map['contactoOS'] as String : null,
+      comentarioOS: map['comentarioOS'] != null ? map['comentarioOS'] as String : null,
       idUser: map['idUser'] != null ? map['idUser'] as int : null,
       idPadron: map['idPadron'] != null ? map['idPadron'] as int : null,
-      idTipoProblema:
-          map['idTipoProblema'] != null ? map['idTipoProblema'] as int : null,
+      idTipoProblema: map['idTipoProblema'] != null ? map['idTipoProblema'] as int : null,
       idMedio: map['idMedio'] != null ? map['idMedio'] as int : null,
-      idCalle: map['idCalle'] != null ? map['idCalle'] as int : null,
-      idColonia: map['idColonia'] != null ? map['idColonia'] as int : null,
-      idUserAsignado:
-          map['idUserAsignado'] != null ? map['idUserAsignado'] as int : null,
+      idUserAsignado: map['idUserAsignado'] != null ? map['idUserAsignado'] as int : null,
     );
   }
 
   String toJson() => json.encode(toMap());
 
-  factory OrdenServicio.fromJson(String source) =>
-      OrdenServicio.fromMap(json.decode(source) as Map<String, dynamic>);
+  factory OrdenServicio.fromJson(String source) => OrdenServicio.fromMap(json.decode(source) as Map<String, dynamic>);
 
   @override
   String toString() {
-    return 'OrdenServicio(idOrdenServicio: $idOrdenServicio, folioOS: $folioOS, contactoOS: $contactoOS, fechaOS: $fechaOS, materialOS: $materialOS, estadoOS: $estadoOS, prioridadOS: $prioridadOS, idUser: $idUser, idPadron: $idPadron, idTipoProblema: $idTipoProblema, idMedio: $idMedio, idCalle: $idCalle, idColonia: $idColonia, idUserAsignado: $idUserAsignado)';
+    return 'OrdenServicio(idOrdenServicio: $idOrdenServicio, folioOS: $folioOS, fechaOS: $fechaOS, materialOS: $materialOS, estadoOS: $estadoOS, prioridadOS: $prioridadOS, contactoOS: $contactoOS, comentarioOS: $comentarioOS, idUser: $idUser, idPadron: $idPadron, idTipoProblema: $idTipoProblema, idMedio: $idMedio, idUserAsignado: $idUserAsignado)';
   }
 
   @override
   bool operator ==(covariant OrdenServicio other) {
     if (identical(this, other)) return true;
-
-    return other.idOrdenServicio == idOrdenServicio &&
-        other.folioOS == folioOS &&
-        other.contactoOS == contactoOS &&
-        other.fechaOS == fechaOS &&
-        other.materialOS == materialOS &&
-        other.estadoOS == estadoOS &&
-        other.prioridadOS == prioridadOS &&
-        other.idUser == idUser &&
-        other.idPadron == idPadron &&
-        other.idTipoProblema == idTipoProblema &&
-        other.idMedio == idMedio &&
-        other.idCalle == idCalle &&
-        other.idColonia == idColonia &&
-        other.idUserAsignado == idUserAsignado;
+  
+    return 
+      other.idOrdenServicio == idOrdenServicio &&
+      other.folioOS == folioOS &&
+      other.fechaOS == fechaOS &&
+      other.materialOS == materialOS &&
+      other.estadoOS == estadoOS &&
+      other.prioridadOS == prioridadOS &&
+      other.contactoOS == contactoOS &&
+      other.comentarioOS == comentarioOS &&
+      other.idUser == idUser &&
+      other.idPadron == idPadron &&
+      other.idTipoProblema == idTipoProblema &&
+      other.idMedio == idMedio &&
+      other.idUserAsignado == idUserAsignado;
   }
 
   @override
   int get hashCode {
     return idOrdenServicio.hashCode ^
-        folioOS.hashCode ^
-        contactoOS.hashCode ^
-        fechaOS.hashCode ^
-        materialOS.hashCode ^
-        estadoOS.hashCode ^
-        prioridadOS.hashCode ^
-        idUser.hashCode ^
-        idPadron.hashCode ^
-        idTipoProblema.hashCode ^
-        idMedio.hashCode ^
-        idCalle.hashCode ^
-        idColonia.hashCode ^
-        idUserAsignado.hashCode;
+      folioOS.hashCode ^
+      fechaOS.hashCode ^
+      materialOS.hashCode ^
+      estadoOS.hashCode ^
+      prioridadOS.hashCode ^
+      contactoOS.hashCode ^
+      comentarioOS.hashCode ^
+      idUser.hashCode ^
+      idPadron.hashCode ^
+      idTipoProblema.hashCode ^
+      idMedio.hashCode ^
+      idUserAsignado.hashCode;
   }
 }
